4ccc98ec41de7aa509ee0671a70421ec1035f23d
Suraj <suraj9629@gmail.com>
2019-07-29 00:11:39 +0530

    Add condition to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
 #TODO: Change the domain after creating heroku app for this api
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "authentication-base-app-api.herokuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "authentication-base-app-api.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
